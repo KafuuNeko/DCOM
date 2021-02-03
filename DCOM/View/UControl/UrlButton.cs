c606f5b2c7ad367ca06e42d6d0f9475810b3568c
Kafuu Neko <kafuuneko@gmail.com>
2021-02-03 20:43:33 +0800

    1.2.6

---
+++ DCOM/View/UControl/UrlButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DCOM.View.UControl
+{
+    public class UrlButton : Button
+    {
+        public string Icon { get; set; }
+
+        public string Tips { get; set; }
+
+        public string Url { get; set; }
+
+    }
+}
